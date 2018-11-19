0d695d887a7386da954e8de70c9d82aae7379d7a
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-11-19 20:33:02 +0100

    feat: start/stop input controller

--- src/core/InputController.ts
+++ src/core/InputController.ts
@@ -6,7 +6,7 @@ export enum Key {
   SPACE = 'SPACE',
   NONE = 'NONE'
 }
-const keyCodeToKey = {
+const KeyCode = {
   40: Key.DOWN,
   38: Key.UP,
   37: Key.LEFT,
@@ -17,14 +17,10 @@ const keyCodeToKey = {
 export type KeyState = Record<Key, boolean>
 
 export class InputController {
-  keyState: KeyState = {
-    [Key.LEFT]: false,
-    [Key.DOWN]: false,
-    [Key.RIGHT]: false,
-    [Key.UP]: false,
-    [Key.SPACE]: false,
-    [Key.NONE]: false
-  }
+  keyState: KeyState = Object.values(Key).reduce((keyState, key) => ({
+    ...keyState, [key]: false
+  }), {})
+  isListening: boolean = false
 
   private static instance: InputController
 
@@ -36,22 +32,41 @@ export class InputController {
   }
 
   private constructor() {
-    window.addEventListener('keydown', evt => {
-      const key = this.getKeyFromKeycode(evt.which)
-      this.keyState[key] = true
-    })
-
-    window.addEventListener('keyup', evt => {
-      const key = this.getKeyFromKeycode(evt.which)
-      this.keyState[key] = false
-    })
   }
 
+  startListening() {
+    if (this.isListening) return
+    
+    window.addEventListener('keydown', this.onKeyDown)
+    window.addEventListener('keyup', this.onKeyUp)
+    
+    this.isListening = true
+  }
+  
+  stopListening() {
+    if (!this.isListening) return
+
+    window.removeEventListener('keydown', this.onKeyDown)
+    window.removeEventListener('keyup', this.onKeyUp)
+    
+    this.isListening = false
+  }  
+
+  onKeyDown = evt => {
+    const key = this.getKeyFromKeyCode(evt.which)
+    this.keyState[key] = true
+  }
+  
+  onKeyUp = evt => {
+    const key = this.getKeyFromKeyCode(evt.which)
+    this.keyState[key] = false
+  }
+  
   isDown(key: Key) {
     return this.keyState[key]
   }
 
-  getKeyFromKeycode(keyCode: number) {
-    return keyCodeToKey[keyCode] || Key.NONE
+  getKeyFromKeyCode(keyCode: number) {
+    return KeyCode[keyCode] || Key.NONE
   }
 }
